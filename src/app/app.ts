b9c1a5167da4032fbd3de2fd69806e52e805a2d1
Audrey <alary.audrey4712@gmail.com>
2017-02-08 16:21:25 +0100

    start project

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app',
+  template: `
+<div class="main-container">
+<app-bar></app-bar>
+<main-container></main-container>
+</div>
+
+  `
+})
+export class App {};
